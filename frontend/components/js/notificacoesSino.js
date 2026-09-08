fdeafe0d754f59847db57a12bd0d1edaadb726ef
Thaynara Falcão <falcaoaudiovisual7@gmail.com>
2026-09-08 13:45:45 -0300

    fix: adição da tela de recuperação de senha via e-mail

--- frontend/components/js/notificacoesSino.js
+++ frontend/components/js/notificacoesSino.js
@@ -1,4 +1,5 @@
 const atualizacoes = [
+    ["🚀", "Agora é possível redefinir a senha!", "Esqueceu a senha? Clique aqui para redefinir e recupere via e-mail.","09/09/2026"],
     ["🚀", "Atualização do sistema", "Novas competências, criação da aba de login e criação de novo usuário, lançamento do Dashboard e lista de colaboradores", "04/09/2026"],
     ["🚀", "Nova versão disponível", "Guia para cada competência e espaço para comentários individuais em cada competência, sininho de notificação de atualizações e layout do PDF muito mais agradável visualmente.", "13/08/2026"],
     ["✨", "Novo layout!", "Novas cores, logo, fonte e design do PDF.", "11/08/2026"],
